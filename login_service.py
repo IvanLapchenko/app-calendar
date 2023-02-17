e88da96cb9b56f2061c2d620676cbdc3ce5c565d
LapchenkoIvan <lapchenko.vanya@gmail.com>
2023-02-17 22:22:20 +0200

    Add login_service to implement loginization in app

---
+++ login_service.py
@@ -0,0 +1,20 @@
+from sqlalchemy import select
+from database import User
+from . import app
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+
+def get_user_by_id(value: any):
+    user = select(User).where(User.id == value)
+    return user if user else None
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return select(User).where(User.id == user_id)
+
