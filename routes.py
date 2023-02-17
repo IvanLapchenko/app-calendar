3053843b86e2eb164627c85ea77de456ac24b20f
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-02-17 17:59:06 +0200

    Add login route

--- routes.py
+++ routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
+from forms import LoginForm, SignupForm
 from . import app
 
-
+@app.route("/")
 @app.route("/main")
 def main():
     return render_template("main.html")
 
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
