5ac93c4f1ca12dbf458ce05dbdb5896f5342d34e
LapchenkoIvan <lapchenko.vanya@gmail.com>
2023-02-28 00:08:22 +0200

    Do refactor

--- routes.py
+++ routes.py
@@ -1,40 +1,46 @@
 from datetime import datetime
 import json
-from flask import request, make_response, jsonify
+from flask import request, make_response
 from .communicate_with_db import add_item_to_db, get_events_for_current_user_by
 from .database import Event
 from . import app
 
 
-def change_format_of_data(data_to_format):
-    request_str_data = data_to_format.decode('utf-8')
-    jsonified_dict = json.loads(request_str_data)
-    jsonified_dict["user"] = 1
-    jsonified_dict["date"] = 1
-    jsonified_dict["time"] = 1
-    return jsonified_dict
+def convert_time_to_object(time_to_format):
+    return datetime.strptime(time_to_format, "%H:%M").time()
+
+
+def convert_date_to_object(date_to_format):
+    return datetime.strptime(date_to_format, "%Y-%m-%d").date()
+
+
+def prepare_data_to_database(data):
+    data = json.loads(data)
+    data["user"] = 1
+    data["date"] = convert_date_to_object(data["date"])
+    data["time"] = convert_time_to_object(data["time"])
+    return data
 
 
 @app.route("/create_event", methods=["POST"])
 def create_event():
     if request.data:
-        print(type(request.data))
-        request_byte_string = request.data
-        jsonified_dict = json.loads(request_byte_string)
-        print(request_byte_string)
-        print(jsonified_dict)
-    else:
-        print(request.data)
-    # event = Event(**data_from_request)
-    # add_item_to_db(event)
-    response = make_response("success")
-    response.status_code = 200
-    return response
+        request_data = prepare_data_to_database(request.data)
+
+        print(request_data)
+        event = Event(**request_data)
+        add_item_to_db(event)
+
+        response = make_response("success")
+        response.status_code = 200
+
+        return response
+    return make_response("there's no data", 400)
 
 
 @app.route("/get_events_by/<date>", methods=["GET"])
 def get_events_by(date):
-    date = "date"
+    date = datetime.fromisoformat(date)
     data = get_events_for_current_user_by(date)
     response = make_response(data)
     return response
